709bb9dabba8a76b6a5342774b92af539ba7a107
buitanhau <hau48912@gmail.com>
2022-12-12 20:11:07 +0700

    message

---
+++ alert_Message.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class alert_Message {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name :");
+        String name = scanner.nextLine();
+        System.out.println("Hello" + name);
+    }
+}
