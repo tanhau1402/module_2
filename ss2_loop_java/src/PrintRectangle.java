4210158ff5c1ce93b94efee36da31f84e86aa03f
buitanhau <hau48912@gmail.com>
2022-12-13 15:28:26 +0700

    update bai tap 13/12/2022

--- ss2_loop_java/src/PrintRectangle.java
+++ ss2_loop_java/src/PrintRectangle.java
@@ -11,32 +11,30 @@ public class PrintRectangle {
         byte select = 0;
         select = scanner.nextByte();
         switch (select) {
-            case 1:
+            case 1 -> {
                 for (int i = 1; i <= 4; i++) {
                     for (int j = 1; j <= 6; j++) {
                         System.out.print("*  ");
                     }
-                    System.out.println("");
+                    System.out.println();
                 }
-                break;
-
-
-            case 2:
-
+            }
+            case 2 -> {
                 for (int i = 1; i <= 6; ++i) {
                     for (int j = 1; j <= i; ++j) {
                         System.out.print("* ");
                     }
                     System.out.println();
                 }
-                break;
-            case 3:
+            }
+            case 3 -> {
                 for (int i = 6; i >= 1; --i) {
                     for (int j = 1; j <= i; ++j) {
                         System.out.print("* ");
                     }
                     System.out.println();
                 }
+            }
         }
     }
 }
