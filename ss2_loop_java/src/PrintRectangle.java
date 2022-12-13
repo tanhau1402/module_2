3930776237dfb45bd6f8b11611acc38c8ce7816f
buitanhau <hau48912@gmail.com>
2022-12-13 15:26:11 +0700

    update bai tap 13/12/202

--- ss2_loop_java/src/PrintRectangle.java
+++ ss2_loop_java/src/PrintRectangle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PrintRectangle {
     public static void main(String[] args) {
         System.out.println("menu");
-        System.out.println("1. Print triangle");
+        System.out.println("1. Print rectangle");
         System.out.println("2. Print square triangle ");
         System.out.println("3. Print isosceles triangle");
         Scanner scanner = new Scanner(System.in);
@@ -12,28 +12,31 @@ public class PrintRectangle {
         select = scanner.nextByte();
         switch (select) {
             case 1:
-                for (int i = 1; i <= 6; i++) {
-                    for (int j = 1; j < i; j++) {
-                        System.out.println("*");
+                for (int i = 1; i <= 4; i++) {
+                    for (int j = 1; j <= 6; j++) {
+                        System.out.print("*  ");
                     }
                     System.out.println("");
                 }
                 break;
-            case 3:
-                int row = 5;
-                int col = 5;
-                for (int i = 0; i < row; i++) {
-                    for (int j = 1; j <= col; j++) {
-                        if (j < (row - i)) {
-                            System.out.println("  ");
-                        } else {
-                            System.out.println("*  ");
-                        }
+
+
+            case 2:
+
+                for (int i = 1; i <= 6; ++i) {
+                    for (int j = 1; j <= i; ++j) {
+                        System.out.print("* ");
                     }
-                    col++;
-                    System.out.println("\n");
+                    System.out.println();
                 }
                 break;
+            case 3:
+                for (int i = 6; i >= 1; --i) {
+                    for (int j = 1; j <= i; ++j) {
+                        System.out.print("* ");
+                    }
+                    System.out.println();
+                }
         }
     }
 }
